eb77f0a767d5eb33184d871bff2f7732b74b2957
rfabreu <raabreugomes@gmail.com>
2022-10-05 10:12:30 -0400

    Created Category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,87 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product
+      }
+    ]
+  })
+    .then(dbCatData => res.json(dbCatData))
+    .catch(err => {
+      res.status(200).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    include: [{ model: Product }],
+    where: { id: req.params.id }
+  })
+    .then(dbCatData => {
+      if (!dbCatData) {
+        res.status(404).json({
+          message: "Couldn't find a category for the ID provided"
+        });
+        return;
+      }
+      res.json(dbCatData);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then(dbCatData => res.json(dbCatData))
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body.category_name, {
+    where: { id: req.params.id }
+  })
+    .then(dbCatData => {
+      if (!dbCatData) {
+        res.status(400).json({
+          message: "Category not found! Try another ID."
+        });
+        return;
+      }
+      res.json(dbCatData);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: { id: req.params.id }
+  })
+    .then(dbCatData => {
+      if (!dbCatData) {
+        res.status(400).json({
+          message: "Category not found! Try another ID"
+        });
+        return;
+      }
+      res.json(dbCatData);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
