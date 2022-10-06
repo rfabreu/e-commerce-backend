4cf9001c9d7024f3f58daacbb161256ace4a495a
rfabreu <raabreugomes@gmail.com>
2022-10-05 21:57:26 -0400

    Updated API routes to fix bug

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -53,9 +53,10 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  Category.update(req.body.category_name, {
-    where: { id: req.params.id }
-  })
+  Category.update({ category_name: req.body.category_name },
+    {
+      where: { id: req.params.id }
+    })
     .then(dbCatData => {
       if (!dbCatData) {
         res.status(400).json({
